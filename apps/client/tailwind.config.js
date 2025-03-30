3afd1b70e1c10a90049f5661610fe684a3239627
b-rva <bir.techlearn@example.com>
2025-03-30 22:31:59 +0530

    Initial commit - frontend and backend setup

---
+++ apps/client/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html", // Path to your HTML files
+    "./src/**/*.{js,ts,jsx,tsx}", // Path to your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
